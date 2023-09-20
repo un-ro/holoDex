6b6cf7a026dfbdb7711496405b5a857ccfcff33a
Unero <ramadhanbhg@gmail.com>
2023-09-20 15:32:24 +0700

    feat: detail screen

--- app/src/main/java/dev/unero/holodex/ui/detail/DetailFragment.kt
+++ app/src/main/java/dev/unero/holodex/ui/detail/DetailFragment.kt
@@ -1,16 +1,19 @@
 package dev.unero.holodex.ui.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dev.unero.holodex.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import dev.unero.holodex.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding as FragmentDetailBinding
+
+    private val args: DetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +21,15 @@ class DetailFragment : Fragment() {
     ): View {
         _binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val actionBar = (requireActivity() as AppCompatActivity).supportActionBar
+        actionBar?.title = args.talent.name
+
+        binding.tvTalentName.text = args.talent.name
     }
 
     override fun onDestroyView() {
