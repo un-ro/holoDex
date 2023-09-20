50445eda8197bd1e71b57d0392e9048a584e55e1
Unero <ramadhanbhg@gmail.com>
2023-09-20 12:14:31 +0700

    add safeargs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,13 @@ plugins {
     id("com.android.application") version "8.2.0-beta04" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
+
+buildscript {
+    repositories {
+        google()
+    }
+
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.2")
+    }
+}
