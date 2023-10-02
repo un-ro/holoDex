aeabf468da79dce955f3bfc1813bf78903f83447
Unero <ramadhanbhg@gmail.com>
2023-10-02 20:37:00 +0700

    feat: about screen

--- app/src/main/java/dev/unero/holodex/ui/about/AboutFragment.kt
+++ app/src/main/java/dev/unero/holodex/ui/about/AboutFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import coil.load
+import coil.transform.CircleCropTransformation
+import dev.unero.holodex.R
 import dev.unero.holodex.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
@@ -17,6 +20,17 @@ class AboutFragment : Fragment() {
     ): View {
         _binding = FragmentAboutBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ivProfile.load(
+            R.drawable.profile
+        ) {
+            transformations(CircleCropTransformation())
+            crossfade(true)
+        }
     }
 
     override fun onDestroyView() {
